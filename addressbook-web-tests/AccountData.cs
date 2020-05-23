c82c051d36039e32399a8bae2c735dbba9cbad9f
Evgeniya <yudina.evgeniya@itmh.ru>
2020-05-23 21:08:50 +0500

    Добавлены классы AccountData и GroupData Добавлены методы через рефакторинг

---
+++ addressbook-web-tests/AccountData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class AccountData
+    {
+        public AccountData(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
