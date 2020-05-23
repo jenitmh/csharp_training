9c943a25d9aab0b0d22204339320c36af4cd0ca1
Evgeniya <yudina.evgeniya@itmh.ru>
2020-05-23 23:04:47 +0500

    Добавлен тест ContactCreationTests, класс ContactData

---
+++ addressbook-web-tests/ContactData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string middlename;
+        private string lastname;
+        private string nickname;
+        private string group;
+
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        public string Middlename
+        {
+            get
+            {
+                return middlename;
+            }
+            set
+            {
+                middlename = value;
+            }
+        }
+
+    }
+}
